57c5819c7c044938914681ee89f1ecab42cbbd5e
SB Components <gajender.singh@sb-components.co.uk>
2021-08-25 15:55:51 +0530

    Create WiFi_TCP_Client.py

---
+++ WiFi_TCP_Client.py
@@ -0,0 +1,47 @@
+# Use thonny ide to program pico
+# Use Hercules SETUP utility for TCP server and client https://www.hw-group.com/software/hercules-setup-utility
+
+from machine import UART, Pin
+import utime,time
+
+WiFi_SSID='Thinkgenix_JioFiber'  # Wifi_SSID
+WiFi_password = '@C3rb3rus'      # WiFi Password
+TCP_ServerIP = '192.168.29.39'   # IP of Computer on which TCP server is running
+Port = '8080'                    # TCP Server Port
+
+uart = UART(0, 115200)           # Default Baud rate
+
+
+######## Function to send or receive commands and data
+
+def sendCMD(cmd,ack,timeout=2000):
+    uart.write(cmd+'\r\n')
+    t = utime.ticks_ms()
+    while (utime.ticks_ms() - t) < timeout:
+        s=uart.read()
+        if(s != None):
+            s=s.decode()
+            print(s)
+            if(s.find(ack) >= 0):
+                return True
+    return False
+#####################################################
+
+uart.write('+++')
+time.sleep(1)
+if(uart.any()>0):uart.read()
+sendCMD("AT","OK")
+sendCMD("AT+CWMODE=3","OK")
+sendCMD("AT+CWJAP=\""+WiFi_SSID+"\",\""+WiFi_password+"\"","OK",20000)
+sendCMD("AT+CIFSR","OK")
+sendCMD("AT+CIPSTART=\"TCP\",\""+TCP_ServerIP+"\","+Port,"OK",10000)
+sendCMD("AT+CIPMODE=1","OK")
+sendCMD("AT+CIPSEND",">")
+
+uart.write('Hello World !!!\r\n')  # Send data to TCP server
+uart.write('ESP8266 TCP Client\r\n')
+while True:
+    s=uart.read()   # receive data from server
+    if(s != None):
+        s=s.decode()
+        print(s)    # Print received data
